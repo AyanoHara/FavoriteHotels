af6583f31e851270f9e15fb8c23ad264fb3b6d7b
Ayano Hara <ayano0127@gmail.com>
2020-12-31 22:18:28 +0900

    first commit

---
+++ FavoriteHotels/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FavoriteHotels
+//
+//  Created by AYANO HARA on 2020/12/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
