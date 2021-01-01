e1d3401bc7fb6bbfff39628a9a887504ffe05aa1
Ayano Hara <ayano0127@gmail.com>
2021-01-01 18:10:58 +0900

    Realmを初期化

--- FavoriteHotels/AppDelegate.swift
+++ FavoriteHotels/AppDelegate.swift
@@ -6,14 +6,19 @@
 //
 
 import UIKit
+import RealmSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+    
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        do {
+        let realm = try Realm()
+        } catch {
+            print("Error initializing new realm, \(error) ")
+        }
         return true
     }
 
