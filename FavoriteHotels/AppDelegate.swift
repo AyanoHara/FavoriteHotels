e0cec360948112d734ae2fff6c3c697c2b2a7eb6
Ayano Hara <ayano0127@gmail.com>
2021-01-02 15:58:27 +0900

    保存先のURLをログにプリントする

--- FavoriteHotels/AppDelegate.swift
+++ FavoriteHotels/AppDelegate.swift
@@ -14,6 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        //Realmの保存先URL
+        print(Realm.Configuration.defaultConfiguration.fileURL)
         do {
         let realm = try Realm()
         } catch {
