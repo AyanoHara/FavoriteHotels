b96cc22a4b629ff492934ab4fb0a321da95b78fd
Ayano Hara <ayano0127@gmail.com>
2021-01-11 23:43:43 +0900

    MapVCを作成

---
+++ FavoriteHotels/Controller/MapViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MapViewController.swift
+//  FavoriteHotels
+//
+//  Created by AYANO HARA on 2021/01/11.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
