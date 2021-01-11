3f4d3fde79f0db39d38ab37a332f9180c6e8d296
Ayano Hara <ayano0127@gmail.com>
2021-01-12 01:25:50 +0900

    現在の位置情報を取得

--- FavoriteHotels/Controller/MapViewController.swift
+++ FavoriteHotels/Controller/MapViewController.swift
@@ -9,20 +9,45 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, MKMapViewDelegate {
     
     let locationManager = CLLocationManager()
     
     @IBOutlet weak var mapView: MKMapView!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        locationManager.delegate = self
         locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        locationManager.requestLocation()
+        mapView.delegate = self
+        mapView.userTrackingMode = MKUserTrackingMode.followWithHeading
+        mapView.showsUserLocation = true
+        
+        // 縮尺を設定
+        var region:MKCoordinateRegion = mapView.region
+        region.center = mapView.userLocation.coordinate
+        
+        region.span.latitudeDelta = 0.02
+        region.span.longitudeDelta = 0.02
+        
+        mapView.setRegion(region,animated:true)
     }
 }
 
-//MARK: - MKMapViewDelegate
-extension MapViewController: MKMapViewDelegate {
+//MARK: - CLLocationManagerDelegate
+extension MapViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            let lat = location.coordinate.latitude
+            let lon = location.coordinate.longitude
+            print(lat)
+            print(lon)
+        }
+    }
     
-    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
 }
