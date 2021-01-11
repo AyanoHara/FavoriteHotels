15f88ce1cbe47226d7ca3de2640eebb628dd9c0c
Ayano Hara <ayano0127@gmail.com>
2021-01-12 00:35:23 +0900

    位置情報に関するユーザー許可のリクエスト

--- FavoriteHotels/Controller/MapViewController.swift
+++ FavoriteHotels/Controller/MapViewController.swift
@@ -7,12 +7,22 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class MapViewController: UIViewController {
+    
+    let locationManager = CLLocationManager()
     
     @IBOutlet weak var mapView: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        locationManager.requestWhenInUseAuthorization()
     }
+}
+
+//MARK: - MKMapViewDelegate
+extension MapViewController: MKMapViewDelegate {
+    
+    
 }
